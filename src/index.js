e67063c8ada3580128e0069c24062a8a6df39fa0
= <shuklashubham2277@gmail.com>
2023-03-26 14:12:22 +0530

    configured socket in our app and done test connection

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+
+// since socket accepts an http server object so that is why we have created that one above
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+io.on('connection', () => {
+    console.log("new socket connection")
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
