9a9932979c71af7f3fbe793c23308d581328673e
= <shuklashubham2277@gmail.com>
2023-04-11 09:27:54 +0530

    added location sharing feature

--- src/index.js
+++ src/index.js
@@ -16,9 +16,18 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
